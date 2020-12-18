cbacf9772a5e6510be0f7fc2b157487fea34276d
kritika_garg <kgarg.kritika@gmail.com>
2020-12-17 19:47:11 -0500

    Forensic study 2

---
+++ assignments/Kgarg/week-16-presentation/Readme.md
@@ -0,0 +1,5 @@
+## Analyzing hashtag squatting by K-pop stans.
+
+### Presentation: https://docs.google.com/presentation/d/1H03xiuv8uncxz6aQcjhNd9h_o2B_dIegYdfquxBimFY/edit?usp=sharing
+
+### Data (downloaded using twint scraping tool): https://drive.google.com/drive/folders/1fFaI6CFIjengfu0NtCPYPJcZ0sEQRKN_?usp=sharing
---
+++ assignments/Kgarg/week-16-presentation/hastag_squatting/archival_data/tweets_wow.s
@@ -0,0 +1,24 @@
+1268142395251740672	therealnemo23	Sorry Sweetie. Black Lives Still Matter. #whiteoutwednesday pic.twitter.com/C9zJVZSVRE
+1268146747773267970	May982837705	I didn't know there's an appreciation day for white out but here are my fave pics of it! Dm me if you want more #whiteoutwednesday pic.twitter.com/JP5Fbi3U4Z
+1268153317873430529	RiceInstant	Shut up whiteouts, what are you gonna do? Erase my pen ink? #whiteoutwednesdaypic.twitter.com/Bet1P6KcwD
+1268154611875151874	ihveanaddiction	Here's your #whiteoutwednesday Racist shitbags.  pic.twitter.com/mHvTJJvBDj
+1268155604071309312	frogboner420	#whiteoutwednesday we must pretect Walmart! pic.twitter.com/ktlAK3jfZl
+1268162681279676416	ClubxCalifornia	#whiteoutwednesdayThis product works well. Idk why everyone going crazy for it. pic.twitter.com/AqbaXCQ6y7
+1268164998188224514	BriannaDelmue	Thank you #kpopstans for taking care of this bullshit hashtag. #whiteoutwednesday pic.twitter.com/CXgdGeYHtr
+1268166898081923077	soloromanoff	anyways acab. sign petitions. donate. #whiteoutwednesday pic.twitter.com/IsAr9ivRUu
+1268167928744009728	shownura	#whiteoutwednesday y’all hear something ? pff ANYWAYS pic.twitter.com/XunDnLEXPT
+1268168254784253952	mycyborgself	the only #whiteoutwednesday we need, is when we rewrite history to support our fellow black communities! pic.twitter.com/CSfjD0juls
+1268175174651785216	FabledCrossing	I'm gonna hijack white out wednesday and show y'all what you're doing. Fuck #whiteoutwednesday pic.twitter.com/tsyLIiP9X7
+1268179039069581313	bonitafrobum	White supremacists watching the #whitelivesmatter and #whiteoutwednesday hashtags being taken over by Kpop stans pic.twitter.com/oFEvpgoV1S
+1268180432677453825	gayfrogteehee69	#whiteoutwednesday um, anyways Stan anonymous  pic.twitter.com/ssWtbGmRXo
+1268180904758910981	sIeazeszn	#whiteoutwednesday kpop stans eating tf out of this, love yall ngl. Anyways sign and donate! pic.twitter.com/0Siujg00ka
+1268181855775440897	TheBigestFandom	The only “whiteout” we need is FUCKING TRUMP OUT OF THE WHITE HOUSE and to the racist who started this trend FUCK YOU #whiteoutwednesday pic.twitter.com/kw1wgu5WHc
+1268187839243698177	Myannaise417	Really thought you were going to let your racist self have  #whiteoutwednesday trend huh? pic.twitter.com/J7piMovSY0
+1268187882264690694	AndreaG27821287	#whiteoutwednesday....... um no shut the fuck  up and stan anonymous pic.twitter.com/OKqOS3PvLi
+1268189371724947456	StanLewis_	Kpop fans are out in full force covering the horrible trending tags and I’m here  for  it  #whiteoutwednesday pic.twitter.com/XBYn9JTddJ
+1268190271092649984	WTFisallathis	I prefer the roll on because it's less messy. #WhiteOutWednesday pic.twitter.com/QUuS4s3JQL
+1268205978282979328	idk_nan_molla_0	The real problem we face with #whiteoutwednesday is choosing which one is the OG. Choose your fighter pic.twitter.com/M1YZ8bBf5W
+1268207507215958018	jiminsouioui	#whiteoutwednesdaySTAN BTS @BTS_twtpic.twitter.com/oo7kxUcrwG
+1268207576241577985	chrxnicIes_	#whiteoutwednesday I thought on Wednesday we wear pink, not white pic.twitter.com/MSDDRSaLI8
+1268216772731170819	HeatherDGM4	For the dumb fucking racists who can’t even spell their racist hashtag correctly #whiteoutwednesday pic.twitter.com/pWBSBXOxE0
+1268223247146840064	TMIPARKJIMIN	Fuck racism bitch, here is a beautiful video of yoongles#whiteoutwednesday pic.twitter.com/xWcMbqlSvG
