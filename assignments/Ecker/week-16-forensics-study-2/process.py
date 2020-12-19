284f307e27ea28ad373ede1bc422aaac44c532ee
Jim Ecker <jecke004@odu.edu>
2020-12-19 02:43:02 -0500

    Fixed process.py

--- assignments/Ecker/week-16-forensics-study-2/process.py
+++ assignments/Ecker/week-16-forensics-study-2/process.py
@@ -1,5 +1,4 @@
 import json
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -29,19 +28,30 @@ def get_hist(data, type=None):
 with open('hashed.json') as f:
     accounts = json.load(f)
 
-types = []
-
-df = pd.DataFrame.from_dict(accounts).T
 scores = []
+types = []
+display_scores = []
+df = pd.DataFrame.from_dict(accounts).T
+
 for account in accounts:
     if 'error' not in accounts[account].keys():
-        scores.append(accounts[account]['cap']['english'])
-        if accounts[account]['cap']['english'] > 0.75:
-            for type in accounts[account]['display_scores']['english']:
-                if type != 'overall':
-                    if accounts[account]['display_scores']['english'][type] > 3:
-                        types.append(type)
+        if accounts[account]['user']['majority_lang'] is 'en':
+            scores.append(accounts[account]['cap']['english'])
+            if accounts[account]['cap']['english'] > 0.75:
+                for type in accounts[account]['display_scores']['english']:
+                    if type != 'overall':
+                        if accounts[account]['display_scores']['english'][type] > 3:
+                            types.append(type)
+            display_scores = df['display_scores']['english']['overall']
+        else:
+            scores.append(accounts[account]['cap']['universal'])
+            if accounts[account]['cap']['unverisal'] > 0.75:
+                for type in accounts[account]['display_scores']['universal']:
+                    if type != 'overall':
+                        if accounts[account]['display_scores']['universal'][type] > 3:
+                            types.append(type)
+            display_scores = df['display_scores']['universal']['overall']
 get_hist(scores, type='cap')
 get_hist(types, type='types')
-get_hist(df['display_scores']['english']['overall'], type='overall_scores')
+get_hist(display_scores, type='overall_scores')
 
